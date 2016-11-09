1acf9b32f149c97b785f69d664715077e283fb95
Jussi Paulamaki <paulamakijussi@gmail.com>
2016-11-09 21:39:33 +0200

    Feature complete

--- src/main/java/app/controllers/UserController.java
+++ src/main/java/app/controllers/UserController.java
@@ -6,18 +6,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import app.database.PostgreUtil;
 import app.models.UserForm;
 
 @Controller
 public class UserController extends WebMvcConfigurerAdapter{
 
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/users").setViewName("users");
-	}
+	private PostgreUtil pgUtil = PostgreUtil.getInstance();
 
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String showForm(UserForm personForm) {
@@ -30,7 +28,14 @@ public class UserController extends WebMvcConfigurerAdapter{
 		if (bindingResult.hasErrors()) {
 			return "userForm";
 		}
-
+		pgUtil.addUser(personForm);
 		return "redirect:/users";
+	}
+
+	@RequestMapping(value="/users", method=RequestMethod.GET)
+	public ModelAndView showUsers(UserForm personForm) {
+		ModelAndView mv = new ModelAndView("/users");
+		mv.addObject("users", PostgreUtil.getInstance().getUsers());
+		return mv;
 	}
 }
---
+++ src/main/java/app/database/PostgreUtil.java
@@ -0,0 +1,97 @@
+package app.database;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.List;
+
+import org.postgresql.Driver;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import app.models.EGender;
+import app.models.UserForm;
+
+public class PostgreUtil {
+
+	private static final PostgreUtil INSTANCE = new PostgreUtil();
+
+	private  JdbcTemplate jdbcTemplate;
+
+	private PostgreUtil(){
+		Driver driver = new Driver();
+		SimpleDriverDataSource ds = new SimpleDriverDataSource(driver,
+				"jdbc:postgresql://localhost:5432/optiscan",
+				"postgres",
+				"password");
+		ds.setDriverClass(org.postgresql.Driver.class);
+		jdbcTemplate = new JdbcTemplate(ds);
+	}
+
+	public static PostgreUtil getInstance(){
+		return INSTANCE;
+	}
+
+	/**
+	 * Creates new table called Users. Drops old table if found.
+	 * @param tableName
+	 */
+	public  void initializeUsersTable(){
+		jdbcTemplate.execute("DROP TABLE IF EXISTS Users");
+		jdbcTemplate.execute("CREATE TABLE Users ("
+				+ "id SERIAL, "
+				+ "first_name VARCHAR(50), "
+				+ "last_name VARCHAR(50),"
+				+ "date_of_birth BIGINT,"
+				+ "gender VARCHAR(6),"
+				+ "why_applying VARCHAR(1000))");
+	}
+
+	/**
+	 * Add new user to Users table.
+	 * @param user
+	 */
+	public void addUser(UserForm user){
+		jdbcTemplate.execute(String.format("INSERT INTO Users ("
+				+ "first_name, "
+				+ "last_name,"
+				+ "date_of_birth,"
+				+ "gender,"
+				+ "why_applying) VALUES ("
+				+ "'%s', '%s', %d, '%s', '%s')",
+				user.getFirstName(),
+				user.getLastName(),
+				user.getDateOfBirth().toInstant().toEpochMilli(),
+				user.getGender().name(),
+				user.getWhyApplying()));
+	}
+
+	/**
+	 * Fetched every row from Users table.
+	 * @return
+	 */
+	public List<UserForm> getUsers(){
+		String query = "SELECT * FROM Users";
+		return jdbcTemplate.query(query, new UserRowMapper());
+	}
+
+	private class UserRowMapper implements RowMapper<UserForm> {
+
+		@Override
+		public UserForm mapRow(ResultSet rs, int rowNum) throws SQLException {
+			UserForm result = new UserForm();
+			result.setFirstName(rs.getString("first_name"));
+			result.setLastName(rs.getString("last_name"));
+			result.setDateOfBirth(Date.from(Instant.ofEpochMilli(rs.getLong("date_of_birth"))));
+			if(rs.getString("gender").equals(EGender.MALE.name())){
+				result.setGender(EGender.MALE);
+			}else{
+				result.setGender(EGender.FEMALE);
+			}
+			result.setWhyApplying(rs.getString("why_applying"));
+			return result;
+		}
+	}
+}
